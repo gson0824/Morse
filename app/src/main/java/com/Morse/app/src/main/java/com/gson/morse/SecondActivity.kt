3c78de3630b6bbc1735ddf187111766d0f739e5e
GyeongJun Son <gson@u.rochester.edu>
2023-02-03 19:08:20 -0500

    First Second Third (added second activity) Fourth (pass input from main activity to second activity)

--- app/src/main/java/com/Morse/app/src/main/java/com/gson/morse/SecondActivity.kt
+++ app/src/main/java/com/Morse/app/src/main/java/com/gson/morse/SecondActivity.kt
@@ -1,11 +1,35 @@
 package com.gson.morse
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
+import org.w3c.dom.Text
+
+const val EXTRA_ANSWER = "com.gson.morse.answer"
 
 class SecondActivity : AppCompatActivity() {
+
+    private lateinit var original: TextView
+    private lateinit var encoded: TextView
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_second2)
+        setContentView(R.layout.activity_second)
+
+        original = findViewById(R.id.original_morse)
+        encoded = findViewById(R.id.encoded_morse)
+
+        val bundle = intent
+        var s = ""
+        if (bundle != null) {
+            s = bundle.getStringExtra("input").toString()
+            original.text = s
+            Toast.makeText(this, s, Toast.LENGTH_LONG).show()
+        }
     }
 }
+
